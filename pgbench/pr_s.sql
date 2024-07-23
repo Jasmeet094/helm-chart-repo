3c99728184fbbd90f35e6f40977f90cc2eb3da13
Jasmeet094 <115403345+Jasmeet094@users.noreply.github.com>
2024-07-23 19:27:04 +0000

    test-new

--- pgbench/pr_s.sql
+++ pgbench/pr_s.sql
@@ -2,16 +2,3 @@ SELECT id AS client_id FROM (
     SELECT id FROM partners_client LIMIT 10000
 ) x ORDER BY RANDOM() LIMIT 1 \gset
 
-SELECT "partners_clientformula"."updated",
-    "partners_clientformula"."created",
-    "partners_clientformula"."id",
-    "partners_clientformula"."client_id",
-    "partners_clientformula"."object_id",
-    "partners_clientformula"."related_object_name",
-    "partners_clientformula"."related_field_name",
-    "partners_clientformula"."result_type",
-    "partners_clientformula"."formula_tokens",
-    "partners_clientformula"."expression_string",
-    "partners_clientformula"."text_description",
-    "partners_clientformula"."recalculation_mode",
-    COALESCE("partners_clientformula"."formula_tokens", NULL) AS "tokens2"
